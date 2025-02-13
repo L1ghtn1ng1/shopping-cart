fadf9eddb163c32512c5d45655f16d5010e56d46
L1ghtn1ng1 <77595914+L1ghtn1ng1@users.noreply.github.com>
2025-02-13 13:55:42 +1300

    layout of and basic styling of footer

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,39 +1,38 @@
-import { useState, useEffect } from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import './App.css'
-import { Outlet } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [items, setItems] = useState([])
-  const [cartItems, setCartItems] = useState([])
+  const [items, setItems] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products?limit=20')
-            .then(res=>res.json())
-            .then((data)=>{
-              // Add a new attribute to each item
-              const updatedData = data.map(item => ({
-                ...item,
-                quantity: 1  // Change this as needed
-              }));
-              setItems(updatedData);
-            });
-        }, []);
+    fetch("https://fakestoreapi.com/products?limit=20")
+      .then((res) => res.json())
+      .then((data) => {
+        // Add a new attribute to each item
+        const updatedData = data.map((item) => ({
+          ...item,
+          quantity: 1, // Change this as needed
+        }));
+        setItems(updatedData);
+      });
+  }, []);
 
   useEffect(() => {
-    console.log(items)
-  }, [items])
-
+    console.log(items);
+  }, [items]);
 
   function addToCart(item, amount = 1, increment = true) {
-   
-    const existingItemIndex = cartItems.findIndex(cartItem => cartItem.id === item.id);
-  
-    if (existingItemIndex !== -1) {
+    const existingItemIndex = cartItems.findIndex(
+      (cartItem) => cartItem.id === item.id
+    );
 
+    if (existingItemIndex !== -1) {
       const updatedCartItems = [...cartItems];
-      if (increment){
+      if (increment) {
         updatedCartItems[existingItemIndex].quantity += amount;
       } else {
         updatedCartItems[existingItemIndex].quantity -= 1;
@@ -44,18 +43,24 @@ function App() {
       setCartItems((prevCartItems) => [...prevCartItems, newItem]);
     }
   }
-  
+
   function deleteItem(item) {
-    setCartItems((prevCartItems) => prevCartItems.filter((cartItem) => cartItem.id !== item.id));
-  } 
+    setCartItems((prevCartItems) =>
+      prevCartItems.filter((cartItem) => cartItem.id !== item.id)
+    );
+  }
 
   return (
     <>
-      <Header cartItems={cartItems} />
-      <Outlet context={{items, addToCart, cartItems, deleteItem}} />
-      <Footer /> 
+      <div className="app-container">
+        <Header cartItems={cartItems} />
+        <Outlet context={{ items, addToCart, cartItems, deleteItem }} />
+        <div className="footer">
+          <Footer />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- shopping-cart/src/components/Footer.jsx
+++ shopping-cart/src/components/Footer.jsx
@@ -1,8 +1,32 @@
+import '../styles/Footer.css'
+import { Link } from 'react-router-dom'
+
 function Footer() {
   return (
-    <footer>
-      <p>Footer</p>
-    </footer>
+    <div className="footer-container">
+      <div className="shoppers">
+        <h3>Shoppers</h3>
+        <Link>About</Link>
+        <Link>Carrers</Link>
+        <Link>Blog</Link>
+        <Link>Sale</Link>
+      </div>
+      <div className="support">
+        <h3>Support</h3>
+        <Link>Contact Us</Link>
+        <Link>FAQs</Link>
+        <Link>Privacy Policy</Link>
+        <Link>Terms and Conditions</Link>
+      </div>
+      <div className="follow">
+        <h3>Follow Us</h3>
+        <Link>Facebook</Link>
+        <Link>Twitter</Link>
+        <Link>Instagram</Link>
+        <Link>Pinterest</Link>
+      </div>
+    </div>
+    
   )
 }
 
